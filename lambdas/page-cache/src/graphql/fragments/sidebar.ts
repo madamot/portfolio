67c4eacf5f4046a374b9e1eaf71c1996fbf3e2ab
Adam Horne <adam.horne@bbc.co.uk>
2024-02-02 14:14:06 +0000

    remove not needed fragments from sidebar fragment

--- lambdas/page-cache/src/graphql/fragments/sidebar.ts
+++ lambdas/page-cache/src/graphql/fragments/sidebar.ts
@@ -7,11 +7,8 @@ import {
 } from '../fragments'
 
 export const SIDEBAR_FRAGMENT = `
-    ${BILLBOARD_FRAGMENT}
     ${BUTTONGROUP_FRAGMENT}
     ${TEXT_FRAGMENT}
-    ${PROMOCARD_FRAGMENT}
-    ${IMAGE_FRAGMENT}
 
     fragment Sidebar on ProjectModelSidebarField {
         ...ButtonGroup
