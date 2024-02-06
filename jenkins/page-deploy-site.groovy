90ec5f5540f04e49a4a1b2207dae1422ce275a00
Adam Horne <adam.horne@bbc.co.uk>
2024-02-06 22:36:15 +0000

    sam install step

--- jenkins/page-deploy-site.groovy
+++ jenkins/page-deploy-site.groovy
@@ -40,6 +40,13 @@ pipeline {
             }
         }
 
+        stage('Install sam-cli') {
+            steps {
+                sh 'python3 -m venv venv && venv/bin/pip install aws-sam-cli'
+                stash includes: '**/venv/**/*', name: 'venv'
+            }
+        }
+
         stage('Build') {
             steps {
                 script {
