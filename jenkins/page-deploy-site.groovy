0d8641fa808a2938384f2408d664805c9e72e39b
Adam Horne <adam.horne@bbc.co.uk>
2024-02-06 22:47:47 +0000

    update groovy file

--- jenkins/page-deploy-site.groovy
+++ jenkins/page-deploy-site.groovy
@@ -30,6 +30,16 @@ pipeline {
     }
 
     stages {
+        stage('Prepare Build Environment') {
+            steps {
+                sh """
+                    python3 -m venv venv
+                    . venv/bin/activate
+                    pip install aws-sam-cli
+                """
+            }
+        }
+
         stage('Test') {
             steps {
                 sh """
@@ -37,13 +47,6 @@ pipeline {
                     yarn
                     yarn run test --passWithNoTests
                 """
-            }
-        }
-
-        stage('Install sam-cli') {
-            steps {
-                sh 'python3 -m venv venv && venv/bin/pip install aws-sam-cli'
-                stash includes: '**/venv/**/*', name: 'venv'
             }
         }
 
