87f8819d24d10aa84b5baf0353f132b3c7ac8712
Adam Horne <adam.horne@bbc.co.uk>
2024-02-18 00:04:15 +0000

    feat: banner styling and icons

--- lambdas/page-deploy-site/src/components/banner/banner.ts
+++ lambdas/page-deploy-site/src/components/banner/banner.ts
@@ -1,5 +1,27 @@
 import { renderComponent } from '../../utils/generator'
 
 export const render = (data: any): string => {
+  switch (data.type) {
+    case 'info':
+      data.icon = '&#9432;'
+      break
+
+    case 'success':
+      data.icon = '&#10003;'
+      break
+
+    case 'danger':
+      data.icon = '&#10006;'
+      break
+
+    case 'warning':
+      data.icon = '&#9888;'
+      break
+
+    default:
+      data.icon = '&#9432;'
+      break
+  }
+
   return renderComponent(data)
 }
