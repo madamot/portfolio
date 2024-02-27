7c70b1808729423ba5846283b1c89bef58a8bf2b
Adam Horne <adam.horne@bbc.co.uk>
2024-02-27 17:33:43 +0000

    feat: add _modelApiKey to fragment

--- lambdas/page-cache/src/graphql/fragments/header.ts
+++ lambdas/page-cache/src/graphql/fragments/header.ts
@@ -1,6 +1,7 @@
 export const HEADER_FRAGMENT = `
     fragment Header on HeaderRecord {
         ... on HeaderRecord {
+            _modelApiKey
             title
             description
         }
