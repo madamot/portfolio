9329da43edf636f05ed9e38bee6b7fbfb204f5ac
Adam Horne <adam.horne@bbc.co.uk>
2024-04-14 20:25:11 +0100

    debug

--- jenkins/deploy-global-navigation.groovy
+++ jenkins/deploy-global-navigation.groovy
@@ -60,7 +60,9 @@ pipeline {
                     withCredentials([aws(credentialsId: "9190845d-626f-4330-88a2-da3508581995")]) {
                         sh """
                             cd ${FILES_PATH}
-                            aws s3 cp ./dist/index.js s3://global-navigation-${ENVIRONMENT} --recursive
+                            cd dist
+                            ls
+                            aws s3 cp index.js s3://global-navigation-${ENVIRONMENT} --recursive
                         """
                     }
                     echo "Global navigation successfully deployed."
