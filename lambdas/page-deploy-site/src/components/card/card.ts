be23b0a9a6cb181577d70c8301306857ca0239c0
Adam Horne <adam.horne@bbc.co.uk>
2024-04-15 23:58:59 +0100

    feat: card variations

--- lambdas/page-deploy-site/src/components/card/card.ts
+++ lambdas/page-deploy-site/src/components/card/card.ts
@@ -1,8 +1,14 @@
 import { renderComponent } from '../../utils/generator'
 import { CardRecord } from '../../generated/graphql'
 
-export const render = (data: CardRecord): string => {
+interface CardStyled extends CardRecord {
+  style: string
+}
+
+export const render = (data: CardStyled): string => {
   data.id = data.id.replace(/-/g, '')
+
+  data.style = 'square'
 
   return renderComponent(data)
 }
