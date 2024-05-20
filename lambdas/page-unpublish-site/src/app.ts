d2cadc05634984403c848b502d0646b67cbdb85a
Adam Horne <adam.horne@bbc.co.uk>
2024-05-20 22:03:21 +0100

    feat: delete cache and page from bucket

--- lambdas/page-unpublish-site/src/app.ts
+++ lambdas/page-unpublish-site/src/app.ts
@@ -1,22 +1,47 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import { S3 } from '@aws-sdk/client-s3'
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- *
- */
+const { AWS_ENV } = process.env
 
-export const handler = async (event: APIGatewayProxyEvent) => {
-  let response: APIGatewayProxyResult
+export const handler = async (event: any) => {
+  const s3 = new S3()
 
   console.log('event', event)
 
   console.time('Overall')
+  const payload = event?.Payload?.payLoad
+
+  console.time('Get page')
+
+  console.timeEnd('Get page')
+
+  console.time('Delete cache')
+
+  await s3.deleteObject({
+    Bucket: `page-${AWS_ENV}-cache`,
+    Key: `${payload.entity.attributes.name}/${payload.entity.attributes.name}.json`,
+  })
+
+  await s3.deleteObject({
+    Bucket: `page-${AWS_ENV}-preview-cache`,
+    Key: `${payload.entity.attributes.name}/${payload.entity.attributes.name}.json`,
+  })
+
+  console.timeEnd('Delete cache')
+
+  console.time('Delete page')
+
+  await s3.deleteObject({
+    Bucket: `page-madamot-${AWS_ENV}`,
+    Key: `${payload.entity.attributes.location}/index.html`,
+  })
+
+  await s3.deleteObject({
+    Bucket: `page-madamot-${AWS_ENV}-preview`,
+    Key: `preview/${payload.entity.attributes.location}/index.html`,
+  })
+
+  console.timeEnd('Delete page')
 
   console.timeEnd('Overall')
 }
