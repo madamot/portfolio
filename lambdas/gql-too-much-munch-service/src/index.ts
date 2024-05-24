700f74c7c42e9e89e1543305a1e7f0ec70f989fe
Adam Horne <adam.horne@bbc.co.uk>
2024-05-24 17:31:24 +0100

    feat: turn old gql handler lambda into toomuchmunch service to be used by app sync

---
+++ lambdas/gql-too-much-munch-service/src/index.ts
@@ -0,0 +1,10 @@
+const { AWS_ENV } = process.env
+
+export const handler = async (event: any) => {
+  console.log('event', event)
+
+  console.time('Overall')
+
+  console.timeEnd('Overall')
+  return 'Hello world!'
+}
