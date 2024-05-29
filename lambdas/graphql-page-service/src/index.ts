f5487f853a1ce008f0af94115de6a09cdb55be78
Adam Horne <adam.horne@bbc.co.uk>
2024-05-29 23:48:46 +0100

    feat: seperating queries in a service

--- lambdas/graphql-page-service/src/index.ts
+++ lambdas/graphql-page-service/src/index.ts
@@ -3,6 +3,21 @@ export const handler = async (event: any) => {
 
   console.time('Overall')
 
+  switch (event.field) {
+    case 'search':
+      return [
+        {
+          id: 1,
+          title: 'Hello world',
+          url: 'www.adamhorne.co.uk',
+        },
+      ]
+    case 'all':
+      return 'You are searching for all'
+    default:
+      throw new Error('Unknown field, unable to resolve ' + event.field)
+  }
+
   console.timeEnd('Overall')
   return { greeting: 'Hello world!', favouriteFood: 'Pizza' }
 }
