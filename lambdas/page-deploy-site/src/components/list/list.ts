db34d06b7cb213b26db44eb8dd86d02863144e0e
Adam Horne <36903785+madamot@users.noreply.github.com>
2024-09-10 17:47:11 +0100

    Revert "Revert "Feat/page/css view transitions"" (#41)
    * Revert "Revert "Feat/page/css view transitions (#38)" (#40)"
    
    This reverts commit 806d10c39bfa02c4950c1141d2ee024bf1008987.
    
    * revert: remove view transition stuff

--- lambdas/page-deploy-site/src/components/list/list.ts
+++ lambdas/page-deploy-site/src/components/list/list.ts
@@ -1,9 +1,22 @@
+import { ContentBoxRecord, ListRecord } from '../../generated/graphql'
 import { loadComponents, renderComponent } from '../../utils/generator'
 
-export const render = (data: any) => {
-  data.items.forEach((item: any) => {
-    item.renderedComponents = loadComponents(item.content)
-  })
+export interface ListBuildData extends ListRecord {
+  items: ContentBoxRender[]
+}
 
-  return renderComponent(data)
+interface ContentBoxRender extends ContentBoxRecord {
+  renderedItem: string
+}
+
+export const render = (data: ListRecord) => {
+  const renderListData: ListBuildData = {
+    ...data,
+    items: data.items.map(item => ({
+      ...item,
+      renderedItem: loadComponents(item.content),
+    })),
+  }
+
+  return renderComponent(renderListData)
 }
