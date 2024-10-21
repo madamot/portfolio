5c1afbc52df85ef654f0b1ed09f1fb5b93d9ce68
Adam Horne <adam.horne@bbc.co.uk>
2024-10-21 23:11:50 +0100

    fix: internal bucket path

--- jenkins/deploy-app.groovy
+++ jenkins/deploy-app.groovy
@@ -19,7 +19,7 @@ pipeline {
     environment {
         DATE = sh(returnStdout: true, script: 'date +%Y-%m-%d_%H.%M.%S').trim()
         FILES_PATH = "${"apps/" + env.APP_NAME}"
-        ASSETS_PATH = "${"/internal/assets/" + env.APP_NAME}"
+        ASSETS_PATH = "${"internal/assets/" + env.APP_NAME}"
     }
 
     options {
@@ -59,11 +59,9 @@ pipeline {
                 script {
                     withCredentials([aws(credentialsId: "9190845d-626f-4330-88a2-da3508581995")]) {
                         sh """
-                            ls
                             cd ${FILES_PATH}
-                            ls
                             aws s3 cp "./dist/index.html" "s3://apps-madamot-${ENVIRONMENT}/${APP_NAME}/index.html"
-                            aws s3 sync "./dist" "s3://apps-internal-madamot-${ENVIRONMENT}/${APP_NAME}/${ASSETS_PATH}" --delete --exclude "index.html"
+                            aws s3 sync "./dist/assets" "s3://apps-internal-madamot-${ENVIRONMENT}/${APP_NAME}/${ASSETS_PATH}" --delete --exclude "index.html"
 
                         """
                     }
