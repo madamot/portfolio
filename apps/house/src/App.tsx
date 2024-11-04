16f0c498b833b5e82e51d3c88c347aca9f5cb7ef
Adam Horne <adam.horne@bbc.co.uk>
2024-11-04 22:17:14 +0000

    feat: display the house model with threejs

--- apps/house/src/App.tsx
+++ apps/house/src/App.tsx
@@ -1,18 +1,20 @@
+import { OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+
+import House from './components/atoms/House'
+
 import './App.css'
-import { Canvas, MeshProps } from '@react-three/fiber'
-import Box from './components/Box'
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    <Canvas>
-      <ambientLight intensity={Math.PI / 2} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Box position={[-1.2, 0, 0] as MeshProps} />
-      <Box position={[1.2, 0, 0] as MeshProps} />
-    </Canvas>
+    <div className="wrapper">
+      <Canvas>
+        {/* <Environment files="/images/round_platform_4k.jpg" /> */}
+        <directionalLight position={[10, 7, -3.5]} intensity={0.5} />
+        <OrbitControls makeDefault />
+        <House />
+      </Canvas>
+    </div>
   )
 }
 
