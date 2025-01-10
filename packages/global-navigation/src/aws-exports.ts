b61c9eb230e1c42a4139f6350f818bb9ee59ec55
Adam Horne <adam.horne@bbc.co.uk>
2025-01-10 23:30:32 +0000

    fix

--- packages/global-navigation/src/aws-exports.ts
+++ packages/global-navigation/src/aws-exports.ts
@@ -3,7 +3,7 @@ const awsmobile = {
     'https://qmv3jp3rezd5pauit6bsah2hay.appsync-api.eu-west-1.amazonaws.com/graphql',
   aws_appsync_region: 'eu-west-1',
   aws_appsync_authenticationType: 'API_KEY',
-  aws_appsync_apiKey: 'da2-ffm6wgk3izclncsxiqouldge2u',
+  aws_appsync_apiKey: 'da2-jjceqwkobrhofblvueczswzc6y',
 }
 
 export default awsmobile
