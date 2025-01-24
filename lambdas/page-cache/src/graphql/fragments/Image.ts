38123dd877a68f17c0f9406aa1ef710f6b177c78
Adam Horne <adam.horne@bbc.co.uk>
2025-01-24 18:40:34 +0000

    feat: add image gql to header

--- lambdas/page-cache/src/graphql/fragments/Image.ts
+++ lambdas/page-cache/src/graphql/fragments/Image.ts
@@ -1,38 +1,38 @@
 export const IMAGE_FRAGMENT = `
     fragment Image on ImageRecord {
         ... on ImageRecord {
+          id
+          _modelApiKey
+          image {
+            _createdAt
+            _updatedAt
+            colors {
+              alpha
+              blue
+              cssRgb
+              green
+              hex
+              red
+            }
+            customData
+            exifInfo
+            filename
+            format
+            md5
+            mimeType
+            size
+            smartTags
+            tags
             id
-            _modelApiKey
-            image {
-              _createdAt
-              _updatedAt
-              colors {
-                alpha
-                blue
-                cssRgb
-                green
-                hex
-                red
-              }
-              customData
-              exifInfo
-              filename
-              format
-              md5
-              mimeType
-              size
-              smartTags
-              tags
-              id
+            alt
+            blurUpThumb
+            responsiveImage {
+              title
               alt
-              blurUpThumb
-              responsiveImage {
-                title
-                alt
-                webpSrcSet
-                src
-              }
+              webpSrcSet
+              src
             }
           }
+        }
     }
 `
--- lambdas/page-cache/src/graphql/fragments/header.ts
+++ lambdas/page-cache/src/graphql/fragments/header.ts
@@ -4,6 +4,40 @@ export const HEADER_FRAGMENT = `
             _modelApiKey
             title
             description
+            image {
+                id
+                _modelApiKey
+                image {
+                    _createdAt
+                    _updatedAt
+                    colors {
+                        alpha
+                        blue
+                        cssRgb
+                        green
+                        hex
+                        red
+                    }
+                    customData
+                    exifInfo
+                    filename
+                    format
+                    md5
+                    mimeType
+                    size
+                    smartTags
+                    tags
+                    id
+                    alt
+                    blurUpThumb
+                    responsiveImage {
+                        title
+                        alt
+                        webpSrcSet
+                        src
+                    }
+                }
+            }
         }
     }
 `
