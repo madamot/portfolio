7fa956ee86b6425fcc7d1200c48bcbc6187af37e
Adam Horne <adam.horne@bbc.co.uk>
2025-01-24 23:45:55 +0000

    fix: replace image dashes

--- lambdas/page-deploy-site/src/components/image/image.ts
+++ lambdas/page-deploy-site/src/components/image/image.ts
@@ -2,5 +2,7 @@ import { renderComponent } from '../../utils/generator'
 import { ImageRecord } from '../../generated/graphql'
 
 export const render = (data: ImageRecord): string => {
+  data.id = data.id.replace(/-/g, '')
+
   return renderComponent(data)
 }
