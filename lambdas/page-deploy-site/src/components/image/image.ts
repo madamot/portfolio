f829137d8208c9d3ddc0a8cbe9757f36a1dff6ad
Adam Horne <adam.horne@bbc.co.uk>
2025-01-25 00:17:39 +0000

    fix image blur

--- lambdas/page-deploy-site/src/components/image/image.ts
+++ lambdas/page-deploy-site/src/components/image/image.ts
@@ -3,6 +3,9 @@ import { ImageRecord } from '../../generated/graphql'
 
 export const render = (data: ImageRecord): string => {
   data.id = data.id.replace(/-/g, '')
+  if (data.image) {
+    data.image.id = data.id
+  }
 
   return renderComponent(data)
 }
