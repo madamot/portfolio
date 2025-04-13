a174921d0f066f9c870a668156e9e7655457dd0b
Adam Horne <36903785+madamot@users.noreply.github.com>
2025-04-13 18:18:27 +0100

    fix: api key bs (#47)

--- packages/global-navigation/src/aws-exports.ts
+++ packages/global-navigation/src/aws-exports.ts
@@ -1,9 +1,13 @@
+const hostname = window.location.hostname
+
 const awsmobile = {
   aws_appsync_graphqlEndpoint:
     'https://qmv3jp3rezd5pauit6bsah2hay.appsync-api.eu-west-1.amazonaws.com/graphql',
   aws_appsync_region: 'eu-west-1',
   aws_appsync_authenticationType: 'API_KEY',
-  aws_appsync_apiKey: 'da2-jjceqwkobrhofblvueczswzc6y',
+  aws_appsync_apiKey: /^((stage)\.)adamhorne\.co.uk/.test(hostname)
+    ? 'da2-nodpnkf3vzbtzoonxyseof5oxq'
+    : 'da2-mtigrs66fvbnphub6ykeontbfa',
 }
 
 export default awsmobile
