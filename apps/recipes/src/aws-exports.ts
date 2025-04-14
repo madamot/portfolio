351dbebfdf243b5eae5c2540dd4c4caf6c6a3262
Adam Horne <adam.horne@bbc.co.uk>
2025-04-14 17:34:07 +0100

    setup

---
+++ apps/recipes/src/aws-exports.ts
@@ -0,0 +1,16 @@
+const hostname = window.location.hostname
+
+console.log('host', hostname)
+
+const isStage = /^((stage)\.)adamhorne\.co.uk/.test(hostname)
+
+const aws = {
+  aws_appsync_graphqlEndpoint: isStage
+    ? 'https://qmv3jp3rezd5pauit6bsah2hay.appsync-api.eu-west-1.amazonaws.com/graphql'
+    : 'https://5jmk2ibtdfasxcnsogfqvk6kem.appsync-api.eu-west-1.amazonaws.com/graphql',
+  aws_appsync_region: 'eu-west-1',
+  aws_appsync_authenticationType: 'API_KEY',
+  aws_appsync_apiKey: isStage ? 'da2-nodpnkf3vzbtzoonxyseof5oxq' : 'da2-mtigrs66fvbnphub6ykeontbfa',
+}
+
+export default aws
--- apps/recipes/src/main.tsx
+++ apps/recipes/src/main.tsx
@@ -2,9 +2,49 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { AuthOptions, createAuthLink } from 'aws-appsync-auth-link'
+import {
+  ApolloClient,
+  ApolloLink,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client'
+
+import appSyncConfig from './aws-exports'
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    PageQueries: {
+      merge: true,
+    },
+    SearchPage: {
+      merge: true,
+    },
+  },
+})
+
+const region = 'eu-west-1'
+
+const auth: AuthOptions = {
+  type: 'API_KEY',
+  apiKey: appSyncConfig.aws_appsync_apiKey,
+  // jwtToken: async () => token, // Required when you use Cognito UserPools OR OpenID Connect. token object is obtained previously
+  // credentials: async () => credentials, // Required when you use IAM-based auth.
+}
+const link = ApolloLink.from([
+  createAuthLink({ url: appSyncConfig.aws_appsync_graphqlEndpoint, region, auth }),
+  createHttpLink({ uri: appSyncConfig.aws_appsync_graphqlEndpoint }),
+])
+const client = new ApolloClient({
+  link,
+  cache,
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </StrictMode>
 )
