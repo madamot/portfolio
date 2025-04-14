9b9d9dbb5479dc619140dd0c7e06e7152887c7e3
Adam Horne <adam.horne@bbc.co.uk>
2025-04-14 09:25:10 +0100

    test

--- packages/global-navigation/src/components/atoms/Logo/Logo.tsx
+++ packages/global-navigation/src/components/atoms/Logo/Logo.tsx
@@ -3,7 +3,7 @@ import './Logo.css'
 const Logo: React.FC = () => {
   return (
     <div className="logo">
-      <a href="/">ADAM HORNE</a>
+      <a href="/">ADAM HORNE - test</a>
     </div>
   )
 }
